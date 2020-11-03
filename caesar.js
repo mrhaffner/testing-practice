08cbbae9499ea3197300f59ea5ea636bdea9d507
Matt Haffner <mrhaffner13@gmail.com>
2020-11-02 21:18:54 -0600

    added a working rot13 caesar cipher and tests

--- caesar.js
+++ caesar.js
@@ -1,5 +1,16 @@
-const caesar = () => {
-    
+const caesar = (str) => {
+    const arr = str.split('').map(x => {
+        if (/[A-Ma-m]/.test(x)) {
+            let shifted = x.charCodeAt(0) + 13
+            return String.fromCharCode(shifted)
+        } else if (/[N-Zn-z]/.test(x)) {
+            let shifted = x.charCodeAt(0) - 13
+            return String.fromCharCode(shifted)
+        } else {
+            return x
+        }
+    })
+    return arr.join('')
 }
 
 module.exports = caesar;
---
+++ caesar.test.js
@@ -0,0 +1,22 @@
+const caesar = require('./caesar')
+
+test('converts to lower case letters', () => {
+    expect(caesar('ma')).toBe('zn');
+});
+
+test('works with uppercase', () => {
+        expect(caesar('MA')).toBe('ZN');
+});
+
+test('works with spaces', () => {
+    expect(caesar('m a')).toBe('z n');
+});
+
+test('works with symbols', () => {
+    expect(caesar('ma!@#$%^&*')).toBe('zn!@#$%^&*');
+});
+
+test('works with letters beyond M', () => {
+    expect(caesar('nz')).toBe('am');
+  });
+ 
